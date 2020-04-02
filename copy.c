03c80c4ed28cc559880ce5669b27bbb073267feb
twbikeman <twbikeman@gmail.com>
2020-04-02 05:23:10 +0000

    finish copy.c

--- copy.c
+++ copy.c
@@ -11,22 +11,23 @@ int main(int argc, const char *argv[]) {
   int fdwrite = open(argv[2], O_APPEND | O_WRONLY | O_CREAT, 0666);
 
   char *buff;
-  ssize_t size;
-
-  /* for(;;) { */
-  /* size = read(fdread, buff, 1); */
-  /* if (size !=1) break; */
-  /* write(fdwrite, buff, 1); */
-  /* } */
-  
+  ssize_t  size;
 
 
 
 
+  // for(;;) { 
+  //size = read(fdread, buff, 1); 
+  // if (size !=1) break; 
+  //write(fdwrite, buff, 1);
+  // }
+;
 
-
-  
-
+ while (1) {
+     size = read(fdread, buff, 1);
+     if (size != 1) break;
+     write(fdwrite, buff, 1);
+    }
   
 
   return 0;
